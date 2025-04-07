de16efca7a20cdd2a18c918e79344df2c6e48a10
Nhi Cung <cunghonglinhnhi@gmail.com>
2025-04-06 20:44:01 -0700

    Updates

--- App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Button } from '@mui/material';
-const App = () => {
-    return (_jsxs("div", { children: [_jsx("h1", { children: "Welcome to my Material UI app!" }), _jsx(Button, { variant: 'contained', color: 'primary', children: "Click Me" })] }));
-};
-export default App;
