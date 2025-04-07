b7eed6bf998dcb5ee55deb8c95f3e29f27630435
Nhi Cung <cunghonglinhnhi@gmail.com>
2025-04-06 19:49:31 -0700

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-"use strict";
-// src/index.ts
-const message = 'Hello, GitHub Pages!';
-document.body.innerHTML = `<h1>${message}</h1>`;
+import { jsx as _jsx } from "react/jsx-runtime";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+const container = document.getElementById('root');
+if (!container)
+    throw new Error('Failed to find the root element');
+const root = createRoot(container);
+root.render(_jsx(React.StrictMode, { children: _jsx(App, {}) }));
